7bd87c3f98e709205b6684253d1a0ab17af8f556
chiayinin <chiayinin@gmail.com>
2024-08-11 19:35:46 +0800

    update: model user

--- models/users.js
+++ models/users.js
@@ -1,18 +1,34 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-  name: {
+  userName: {
     type: String,
     required: [true, '請輸入您的名字']
   },
   email: {
     type: String,
     required: [true, '請輸入您的 Email'],
-    unique: true,
-    lowercase: true,
+    unique: true, // 確保 email 欄位是唯一值，重複插入會拋錯
+    lowercase: true, // 將欄位值轉為小寫
     select: false
   },
-  photo: String,
+  photo: {
+    type: String,
+    default: ''
+  },
+  createDate: {
+    type: Number
+  },
+  updateDate: {
+    type: Number,
+  }
 });
+const userOption = {
+  versionKey: false, // 禁用 Mongoose 自動添加的 __v 版本
+  timestamps: {
+    createdAt: 'createDate',
+    updatedAt: 'updateDate'
+  }
+}
 const User = mongoose.model('users', userSchema);
 
 module.exports = User;
